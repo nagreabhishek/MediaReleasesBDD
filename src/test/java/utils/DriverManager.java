15d1bb47ce64e7bc358df80eb96060a8dc8e8df4
Abhishek Nagre <nagre.abhishek@yahoo.com>
2024-07-16 12:00:36 +1000

    Updated files

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -2,6 +2,7 @@ package utils;
 
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,28 +11,34 @@ import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 import java.net.MalformedURLException;
 
-public class DriverManager extends Base{
+public class DriverManager extends Base {
 
-	public static PropertiesReader readConfig = new PropertiesReader();
-	public static String browserName = readConfig.getPropertyValue("BROWSER");
-	public static String baseURL = readConfig.getPropertyValue("URL");
+    public static PropertiesReader readConfig = new PropertiesReader();
+    public static String browserName = readConfig.getPropertyValue("BROWSER");
+    public static String baseURL = readConfig.getPropertyValue("URL");
 
-	public static void launchBrowser() throws MalformedURLException {
-		try {
+    public static void launchBrowser() throws MalformedURLException {
+        try {
             Configuration.browser = browserName;
-            Configuration.browserSize = "1920x1080";
+            if (browserName.equalsIgnoreCase("chrome")) {
+                Configuration.browserSize = "1920x1080";
+            }
             Configuration.headless = false;
             if ("CHROME".equals(browserName.toUpperCase())) {
                 Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
             }
-			open(baseURL);
-			logger.info("Browser Launched Successfully");
-		} catch (Exception e) {
-			logger.error("Error while opening browser. Browser Name = " + browserName);
-			e.printStackTrace();
-		}
-	}
+            open(baseURL);
+            if (browserName.equalsIgnoreCase("safari")) {
+                WebDriverRunner.getWebDriver().manage().window().maximize();
+            }
+            logger.info("Browser Launched Successfully");
+        } catch (Exception e) {
+            logger.error("Error while opening browser. Browser Name = " + browserName);
+            e.printStackTrace();
+        }
+    }
 }
